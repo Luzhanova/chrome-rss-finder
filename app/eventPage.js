ca32b162d2aa9b29ba8b7091f42fdf2b53845915
ML <lum.flow@gmail.com>
2017-08-20 17:54:04 +0200

    Empty basis of plugin

---
+++ app/eventPage.js
@@ -0,0 +1,13 @@
+'use strict';
+
+chrome.commands.onCommand.addListener(function (command) {
+
+});
+
+chrome.runtime.onInstalled.addListener(function () {
+
+});
+
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+
+});
